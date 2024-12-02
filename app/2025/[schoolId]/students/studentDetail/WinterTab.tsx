9cdabc0ebc6db355630f999b9b426d9843a9e962
kiseon77 <kieson.han77@gmail.com>
2024-12-02 23:51:42 +0900

    feat:학생상세, 동복상세

--- app/2025/[schoolId]/students/studentDetail/WinterTab.tsx
+++ app/2025/[schoolId]/students/studentDetail/WinterTab.tsx
@@ -3,19 +3,38 @@ import {
   Typography,
   List,
   ListItemButton,
-  ListItemIcon,
   ListItemText,
+  IconButton,
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
 } from "@mui/material";
-import React from "react";
-import dayjs, { Dayjs } from "dayjs";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-export default function WinterTab() {
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
-  const [value, setValue] = React.useState<Dayjs | null>(dayjs("2022-04-17"));
+import React, { useState } from "react";
+import { Add } from "@mui/icons-material";
+import Image from "next/image";
 
+function createUniformData(
+  uniform: string,
+  size: number,
+  quantity: number,
+  orderCount?: number,
+) {
+  return { uniform, size, quantity, orderCount };
+}
+function createAccData(name: string, size: string, quantity: number) {
+  return { name, size, quantity };
+}
+const UniformRows = [createUniformData("Frozen yoghurt", 159, 6.0, 1)];
+const NameTagRows = [1, 2, 3, 4, 5];
+const AccRows = [createAccData("Frozen yoghurt", "size", 159)];
+
+export default function WinterTab({ data }: any) {
+  const [selectedIndex, setSelectedIndex] = useState(1);
   const handleListItemClick = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>,
     index: number,
@@ -44,8 +63,110 @@ export default function WinterTab() {
             >
               <ListItemText primary="Trash" />
             </ListItemButton>
+            <ListItemButton
+              selected={selectedIndex === 3}
+              onClick={(event) => handleListItemClick(event, 3)}
+            >
+              <Add />
+            </ListItemButton>
           </Stack>
         </List>
+      </Stack>
+      <Stack
+        direction="row"
+        spacing={2}
+        sx={{
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+        }}
+      >
+        <Box>
+          <Typography variant="body2" gutterBottom>
+            교복 {data.price.toLocaleString("ko-KR")}원
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableBody>
+                {UniformRows.map((row) => (
+                  <TableRow
+                    key={row.uniform}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.uniform}
+                    </TableCell>
+                    <TableCell align="right">{row.size}</TableCell>
+                    <TableCell align="right">{row.quantity}</TableCell>
+                    <TableCell align="right">{row.orderCount}차</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+        <Box>
+          <Typography variant="body2" gutterBottom>
+            명찰 {data.price.toLocaleString("ko-KR")}원
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableBody>
+                {NameTagRows.map((row) => (
+                  <TableRow
+                    key={row}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row ? row : "-"}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+        <Box>
+          <Typography variant="body2" gutterBottom>
+            용품 {data.price.toLocaleString("ko-KR")}원
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableBody>
+                {AccRows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.size}</TableCell>
+                    <TableCell align="right">{row.quantity}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+        <Box>
+          <Typography variant="body2" gutterBottom>
+            수령 사인
+          </Typography>
+          <Box>
+            <Image
+              src="/profile.png"
+              width={60}
+              height={60}
+              alt="Picture of the author"
+            />
+          </Box>
+        </Box>
       </Stack>
     </>
   );
--- app/2025/[schoolId]/students/studentDetail/page.tsx
+++ app/2025/[schoolId]/students/studentDetail/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import * as React from "react";
+import React, { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import {
   Box,
@@ -21,9 +21,12 @@ import { TabContext, TabList, TabPanel } from "@mui/lab";
 import WinterTab from "./WinterTab";
 
 export default function StudentDetails() {
-  const [value, setValue] = React.useState("1");
+  const [value, setValue] = useState("1");
   const searchParams = useSearchParams();
   const studentId = searchParams.get("studentId");
+  const [data, setData] = useState([
+    { type: "winter", price: 393000, uniform: [] },
+  ]);
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
@@ -152,7 +155,7 @@ export default function StudentDetails() {
               </TabList>
             </Box>
             <TabPanel value="1">
-              <WinterTab />
+              <WinterTab data={data[0]} />
             </TabPanel>
             <TabPanel value="2">Item Two</TabPanel>
             <TabPanel value="3">Item Three</TabPanel>
